94adb9d170a7673e7af625d89ee213685eaa8959
rahmaAlmamari <alr816125@gmail.com>
2025-08-05 08:32:48 +0400

    Adding LoadAccounts method to file context

--- FileContext.cs
+++ FileContext.cs
@@ -1,13 +1,25 @@
-﻿using System;
+﻿using BankSystem_LevelArchitecture.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace BankSystem_LevelArchitecture
 {
     class FileContext
     {
+        //file path ...
         private static string filePath = "accounts.json";
+        //to load bank accounts from a file
+        public static List<BankAccount> LoadAccounts()
+        {
+            if (!File.Exists(filePath))
+                return new List<BankAccount>();
+
+            var json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<BankAccount>>(json) ?? new List<BankAccount>();
+        }
     }
 }
