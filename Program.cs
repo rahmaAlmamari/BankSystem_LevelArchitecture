b56f927493cb21674c1f2d8de49abac45f3da4e3
rahmaAlmamari <alr816125@gmail.com>
2025-08-05 09:30:01 +0400

    create a new instance of the repository in the main method

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace BankSystem_LevelArchitecture
+﻿using BankSystem_LevelArchitecture.Repostries;
+
+namespace BankSystem_LevelArchitecture
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            //to create a new instance of the repository ...
+            IBankAccountRepository repo = new BankAccountRepository();
+            //to create a new instance of the service ...
+            //IBankService service = new BankService(repo);
         }
     }
 }
